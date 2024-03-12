13211f3885686352aa8587dd51d75330380df29c
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-03-12 14:51:57 +0200

    12/03/2024

---
+++ Calendar/main.py
@@ -0,0 +1,107 @@
+import sys
+from custome_errors import *
+sys.excepthook = my_excepthook
+from hijri_converter import convert
+import update
+import gui
+import guiTools
+from settings import *
+import PyQt6.QtWidgets as qt
+import PyQt6.QtGui as qt1
+import PyQt6.QtCore as qt2
+language.init_translation()
+class main (qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(app.name + _("version : ") + str(app.version))
+        layout=qt.QVBoxLayout()
+        self.select=qt.QComboBox()
+        self.select.addItems([_("from AD to hijri"),_("from hijri to AD")])
+        layout.addWidget(self.select)
+        self.year=qt.QSpinBox()
+        self.year.setRange(1,10000)
+        self.year.setAccessibleName(_("year"))
+        layout.addWidget(self.year)
+        self.month=qt.QSpinBox()
+        self.month.setRange(1,12)
+        self.month.setAccessibleName(_("month"))
+        layout.addWidget(self.month)
+        self.day=qt.QSpinBox()
+        self.day.setRange(1,31)
+        self.day.setAccessibleName(_("day"))
+        layout.addWidget(self.day)
+        self.convert=qt.QPushButton(_("convert"))
+        self.convert.setDefault(True)
+        self.convert.clicked.connect(self.on_convert)
+        layout.addWidget(self.convert)
+        self.setting=qt.QPushButton(_("settings"))
+        self.setting.setDefault(True)
+        self.setting.clicked.connect(lambda: settings(self).exec())
+        layout.addWidget(self.setting)
+        w=qt.QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)
+
+        mb=self.menuBar()
+        help=mb.addMenu(_("help"))
+        helpFile=qt1.QAction(_("help file"),self)
+        help.addAction(helpFile)
+        helpFile.triggered.connect(lambda:guiTools.HelpFile())
+        helpFile.setShortcut("f1")
+        cus=help.addMenu(_("contact us"))
+        telegram=qt1.QAction("telegram",self)
+        cus.addAction(telegram)
+        telegram.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/mesteranasm"))
+        telegramc=qt1.QAction(_("telegram channel"),self)
+        cus.addAction(telegramc)
+        telegramc.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/tprogrammers"))
+        githup=qt1.QAction(_("Github"),self)
+        cus.addAction(githup)
+        githup.triggered.connect(lambda: guiTools.OpenLink(self,"https://Github.com/mesteranas"))
+        X=qt1.QAction(_("x"),self)
+        cus.addAction(X)
+        X.triggered.connect(lambda:guiTools.OpenLink(self,"https://x.com/mesteranasm"))
+        email=qt1.QAction(_("email"),self)
+        cus.addAction(email)
+        email.triggered.connect(lambda: guiTools.sendEmail("anasformohammed@gmail.com","project_type=GUI app={} version={}".format(app.name,app.version),""))
+        Github_project=qt1.QAction(_("visite project on Github"),self)
+        help.addAction(Github_project)
+        Github_project.triggered.connect(lambda:guiTools.OpenLink(self,"https://Github.com/mesteranas/{}".format(settings_handler.appName)))
+        Checkupdate=qt1.QAction(_("check for update"),self)
+        help.addAction(Checkupdate)
+        Checkupdate.triggered.connect(lambda:update.check(self))
+        licence=qt1.QAction(_("license"),self)
+        help.addAction(licence)
+        licence.triggered.connect(lambda: Licence(self))
+        donate=qt1.QAction(_("donate"),self)
+        help.addAction(donate)
+        donate.triggered.connect(lambda:guiTools.OpenLink(self,"https://www.paypal.me/AMohammed231"))
+        about=qt1.QAction(_("about"),self)
+        help.addAction(about)
+        about.triggered.connect(lambda:qt.QMessageBox.information(self,_("about"),_("{} version: {} description: {} developer: {}").format(app.name,str(app.version),app.description,app.creater)))
+        self.setMenuBar(mb)
+        if settings_handler.get("update","autoCheck")=="True":
+            update.check(self,message=False)
+    def closeEvent(self, event):
+        if settings_handler.get("g","exitDialog")=="True":
+            m=guiTools.ExitApp(self)
+            m.exec()
+            if m:
+                event.ignore()
+        else:
+            self.close()
+    def on_convert(self):
+        index=self.select.currentIndex()
+        year=self.year.value()
+        month=self.month.value()
+        day=self.day.value()
+        if index==0:
+            result=convert.Gregorian(year,month,day).to_hijri()
+        else:
+            result=convert.Hijri(year,month,day).to_gregorian()
+        qt.QMessageBox.information(self,_("result"),str(result))
+App=qt.QApplication([])
+w=main()
+w.show()
+App.setStyle('fusion')
+App.exec()
---
+++ Calendar/settings/app.py
@@ -0,0 +1,6 @@
+name="Calendar"
+appName="Calendar_GUI"
+appdirname="Calendar"
+version=1.0
+description="A tool to convert between calendars"
+creater="mesteranas"
